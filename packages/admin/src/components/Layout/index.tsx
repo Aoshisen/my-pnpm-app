e058abb06d057b327388dc9c6bfc985afbab843e
aoshisen <aoshisen.23@gmail.com>
2025-02-26 23:50:03 +0800

    feat: 更语义化一些

--- packages/admin/src/components/Layout/index.tsx
+++ packages/admin/src/components/Layout/index.tsx
@@ -8,11 +8,10 @@ type LayoutProps<T = ReactNode> = {
 const Layout: FC<PropsWithChildren<LayoutProps>> = ({ children, header, side }) => {
 	return (
 		<div id="layout" className="p-4 bg-gray-1 h-screen">
-			<div className="grid grid-rows-[62px_1fr]  grid-cols-[380px_10px_1fr] h-full shadow-lg rounded-b-md overflow-hidden">
+			<div className="grid grid-rows-[62px_1fr]  grid-cols-[380px_1fr] h-full shadow-lg rounded-b-md overflow-hidden">
 				<header className="col-span-full bg-white overflow-y-auto  shadow-sm z-1">{header}</header>
-				<div className="bg-white overflow-y-auto">{side}</div>
-				<span></span>
-				<div className=" bg-white overflow-y-auto">{children}</div>
+				<aside className="bg-white overflow-y-auto">{side}</aside>
+				<main className="overflow-y-auto bg-white m-2">{children}</main>
 			</div>
 		</div>
 	)
