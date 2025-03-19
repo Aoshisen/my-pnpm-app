70238919183d22825ab94cc8f956d9b63a81177a
aoshisen <aoshisen.23@gmail.com>
2025-03-19 15:54:56 +0800

    style: 样式优化

--- packages/admin/src/pages/Test/Test.tsx
+++ packages/admin/src/pages/Test/Test.tsx
@@ -1,8 +1,8 @@
 import { CSSProperties } from "react";
 import { randomInt, hierarchy, pack, HierarchyNode, descending, randomGeometri, randomUniform, randomLogNormal } from "d3";
 function createData() {
-	const randomFunc=randomLogNormal(0,1)
-	const list = Array.from({ length: 1000 }).fill(".").map(() => randomFunc()).map((i, d) => ({ id: d, value: i }))
+	const randomFunc = randomLogNormal(0, 1)
+	const list = Array.from({ length: 100 }).fill(".").map(() => randomFunc()).map((i, d) => ({ id: d, value: i }))
 	const data = hierarchy(
 		{
 			name: "parent",
@@ -10,21 +10,22 @@ function createData() {
 		}
 	) as HierarchyNode<unknown>;
 
-	return pack().size([800, 800])(data.sum(d => d.value)).sort((a, b) => descending(a.value, b.value));
+	return pack().size([800, 800]).padding(2)(data.sum(d => d.value)).sort((a, b) => descending(a.value, b.value));
 }
 const Test = () => {
 	const data = createData();
-	const className = "pos-absolute border-black  border origin-center rounded-full"
+	const className = "pos-absolute border-black  border  rounded-full"
+	const circleClassName = " border-black  border  rounded-full";
 	return (
-		<div className="position-relative">
-			<div className={className} style={{ width: 800, height: 800 }}></div>
+		<div className="position-relative w-max m-x-auto m-t-10">
+			<div style={{ width: 800, height: 800 }} className={circleClassName}></div>
 			{
 				data.children?.map((item) => {
 					const styles = {
 						width: item.r * 2,
 						height: item.r * 2,
-						left: item.x-item.r,
-						top: item.y-item.r,
+						left: item.x - item.r,
+						top: item.y - item.r,
 					} as CSSProperties;
 					return <div key={item.data?.id}
 						className={className}
